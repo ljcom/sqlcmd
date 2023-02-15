0b647be51a046b9332ab24a556f430a6336cd61c
Samuel Surya <samuel.surya@gmail.com>
2023-02-15 14:20:29 +0700

    .

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
 FROM node:18.0.0
 
-WORKDIR /var/opt/mssql/data
-
-#RUN touch text.txt
-# RUN touch egccrm_data.mdf
-# RUN touch egccrm_data_log.ldf
-# RUN touch egccrm_v4.mdf
-# RUN touch egccrm_v4_log.ldf
-
 WORKDIR /var/opt/mssql/source
 
 COPY *.bak .
@@ -22,8 +14,5 @@ RUN apt-get update
 
 RUN npm install
 
-#CMD node index.js -S:mssql,1433 -U:sa -P:SA_Passw0rd -D:master -C -N -i:./filldata.sql
-
-#CMD node index.js -S:mssql,1433 -U:sa -P:SA_Passw0rd -D:master -C -N
 CMD node index.js -S:mssql -U:sa -P:SA_Passw0rd -D:master -C -N -i:restore.sql
 
--- restore.sql
+++ restore.sql
@@ -13,20 +13,20 @@ if not exists(select * from sys.databases where name='egccrm_data') begin
         MOVE 'egccrm_data_log' TO '/var/opt/mssql/source/egccrm_data_log.ldf', REPLACE
 end 
 
-if not exists(select * from sys.databases where name='egccrm_v4') begin
+-- if not exists(select * from sys.databases where name='egccrm_v4') begin
 
-    CREATE DATABASE [egccrm_v4] ON  PRIMARY 
-        ( NAME = N'egccrm_v4', FILENAME = N'/var/opt/mssql/source/egccrm_v4.mdf' , 
-            SIZE = 167872KB , MAXSIZE = UNLIMITED, FILEGROWTH = 16384KB )
-        LOG ON 
-        ( NAME = N'egccrm_v4_log', FILENAME = N'/var/opt/mssql/source/egccrm_v4_log.ldf' , 
-            SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 16384KB )
+--     CREATE DATABASE [egccrm_v4] ON  PRIMARY 
+--         ( NAME = N'egccrm_v4', FILENAME = N'/var/opt/mssql/source/egccrm_v4.mdf' , 
+--             SIZE = 167872KB , MAXSIZE = UNLIMITED, FILEGROWTH = 16384KB )
+--         LOG ON 
+--         ( NAME = N'egccrm_v4_log', FILENAME = N'/var/opt/mssql/source/egccrm_v4_log.ldf' , 
+--             SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 16384KB )
 
 
-    restore database egccrm_v4
-    from disk='../source/egccrm_v4_230211.bak'
-    WITH 
-        MOVE 'egccrm_v4' TO '/var/opt/mssql/source/egccrm_v4.mdf',
-        MOVE 'egccrm_v4_log' TO '/var/opt/mssql/source/egccrm_v4_log.ldf', REPLACE
-END
+--     restore database egccrm_v4
+--     from disk='../source/egccrm_v4_230211.bak'
+--     WITH 
+--         MOVE 'egccrm_v4' TO '/var/opt/mssql/source/egccrm_v4.mdf',
+--         MOVE 'egccrm_v4_log' TO '/var/opt/mssql/source/egccrm_v4_log.ldf', REPLACE
+-- END
 
